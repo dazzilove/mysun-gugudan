10c14785c2ca125bb7076bcb23d9b93b774dbbb8
dazzilove <dazzilove@gmail.com>
2018-05-24 12:58:27 +0900

    confirm button, alert message, random list commit

--- mysun-gugudan-viewer-ipad/mysun-gugudan-viewer-ipad/ViewController.swift
+++ mysun-gugudan-viewer-ipad/mysun-gugudan-viewer-ipad/ViewController.swift
@@ -17,20 +17,17 @@ class ViewController: UIViewController {
     @IBOutlet weak var lblQuestion: UILabel!
     @IBOutlet weak var lblAlertText: UILabel!
     @IBOutlet weak var lblQuestionNumber: UILabel!
-
+    @IBOutlet weak var btnConfirm: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        lblDateTitle.text = makeDateTitle();
-        lblAlertText.text = "확인을 클릭하여 다음 문제로 넘어가세요.";
-        
-        questionList = makeQuesionList()
-        lblQuestion.text = makeQuestion(orgText: questionList[3]);
-        questionNumber = 1;
-        lblQuestionNumber.text = makeQuesionPosition();
+        lblDateTitle.text = makeDateTitle()
+        questionList = makeRandomQuesionList()
+        setQuestionInfo()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -55,7 +52,27 @@ class ViewController: UIViewController {
     
     func makeQuesionPosition() -> String {
         let questionListSize = questionList.count
-        return String("\(questionNumber)/\(questionListSize)");
+        return String("\(questionNumber + 1)/\(questionListSize)");
+    }
+    
+    func makeAlertText() -> Void {
+        let alertTextMessageDoing = "확인을 클릭하여 다음 문제로 넘어가세요.";
+        let alertTextMessageDone = "오늘 문제를 모두 풀었습니다. 수고하셨습니다. ^^";
+        
+        let questionListSize = questionList.count
+        var alertText = "";
+        var alertTextColor = UIColor.black;
+        
+        if (questionListSize == (questionNumber + 1)) {
+            alertText = alertTextMessageDone;
+            alertTextColor = UIColor.blue
+        } else {
+            alertText = alertTextMessageDoing;
+            alertTextColor = UIColor.black
+        }
+        
+        lblAlertText.text = alertText
+        lblAlertText.textColor = alertTextColor;
     }
     
     func makeQuesionList() -> Array<String> {
@@ -73,8 +90,41 @@ class ViewController: UIViewController {
         
         return questionList
     }
-
-
-
+    
+    func makeRandomQuesionList() -> Array<String> {
+        var questionList = makeQuesionList()
+        var randomList = Array<String>()
+        
+        let questionListSize = questionList.count
+        for _ in 0..<questionListSize {
+            let randomIndex = Int(arc4random_uniform(UInt32(questionList.count)) + 1) - 1;
+            let randomQuesion = questionList[randomIndex]
+            questionList.remove(at: randomIndex)
+            randomList.append(randomQuesion)
+        }
+        
+        return randomList
+    }
+    
+    func setQuestionInfo() {
+        let questionListSize = questionList.count
+        if (questionListSize == questionNumber) {
+            return
+        }
+        
+        lblQuestion.text = makeQuestion(orgText: questionList[questionNumber]);
+        lblQuestionNumber.text = makeQuesionPosition()
+        makeAlertText()
+        
+        questionNumber = questionNumber + 1;
+    }
+    
+    @IBAction func btnConfirmOnClick(_ sender: UIButton) {
+        setQuestionInfo()
+    }
+    
+    
+    
 }
+
 
