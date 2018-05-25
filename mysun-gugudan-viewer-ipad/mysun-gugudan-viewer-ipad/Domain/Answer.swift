0effcceb0640f82867bbe4444ae7f93d5344bd54
dazzilove <dazzilove@gmail.com>
2018-05-25 12:48:47 +0900

    first commit

---
+++ mysun-gugudan-viewer-ipad/mysun-gugudan-viewer-ipad/Domain/Answer.swift
@@ -0,0 +1,20 @@
+//
+//  Answer.swift
+//  mysun-gugudan-viewer-ipad
+//
+//  Created by 10028467 on 2018. 5. 25..
+//  Copyright © 2018년 dazzilove. All rights reserved.
+//
+
+import Foundation
+
+class Answer {
+    let quesion: String
+    var answer: String
+    var startTime: Date
+    var endTime: Date
+    
+    init(question: String) {
+        self.quesion = quesion
+    }
+}
